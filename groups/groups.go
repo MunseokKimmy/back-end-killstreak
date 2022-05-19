a96c1edb54590257db37970ce51ccafa90937e6b
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-05-19 09:41:21 -0600

    Add groups package.

---
+++ groups/groups.go
@@ -0,0 +1,45 @@
+package groups
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Group struct {
+	groupid           int
+	name              string
+	datecreated       time.Time
+	gamelastcompleted time.Time
+}
+
+func GetAllGroups(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM groups")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		http.Error(w, "12", 500)
+		return
+	}
+	defer rows.Close()
+
+	groups := make([]*Group, 0)
+	for rows.Next() {
+		group := new(Group)
+		err := rows.Scan(&group.groupid, &group.name, &group.datecreated, &group.gamelastcompleted)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		groups = append(groups, group)
+	}
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		http.Error(w, "36", 500)
+		return
+	}
+	for _, group := range groups {
+		fmt.Fprintf(w, "Group ID: %d, Name: %s, Date Created: %s, Game Last Completed: %s\n", group.groupid, group.name, group.datecreated.Format("2006-01-02"), group.gamelastcompleted.Format("2006-01-02"))
+	}
+}
--- main.go
+++ main.go
@@ -5,18 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
+	"practice/groups"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
-
-type Group struct {
-	groupid           int
-	name              string
-	datecreated       time.Time
-	gamelastcompleted time.Time
-}
 
 var db *sql.DB
 
@@ -51,34 +44,7 @@ func groupsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
-
-	rows, err := db.Query("SELECT * FROM groups")
-	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		http.Error(w, "12", 500)
-		return
-	}
-	defer rows.Close()
-
-	groups := make([]*Group, 0)
-	for rows.Next() {
-		group := new(Group)
-		err := rows.Scan(&group.groupid, &group.name, &group.datecreated, &group.gamelastcompleted)
-		if err != nil {
-			http.Error(w, http.StatusText(500), 500)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		groups = append(groups, group)
-	}
-	if err = rows.Err(); err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		http.Error(w, "36", 500)
-		return
-	}
-	for _, group := range groups {
-		fmt.Fprintf(w, "Group ID: %d, Name: %s, Date Created: %s, Game Last Completed: %s\n", group.groupid, group.name, group.datecreated.Format("2006-01-02"), group.gamelastcompleted.Format("2006-01-02"))
-	}
+	groups.GetAllGroups(db, w, r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
