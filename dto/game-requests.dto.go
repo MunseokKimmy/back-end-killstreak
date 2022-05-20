8005e82f120970ab301ddf12a319e30c50556fa6
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-05-20 12:33:16 -0600

    Add game request dtos.

---
+++ dto/game-requests.dto.go
@@ -0,0 +1,86 @@
+package dto
+
+import "time"
+
+/*
+
+ */
+type GetGameRequest struct {
+	GameId int `json:"gameid"`
+}
+
+/*
+	/game/creategame/
+	Creates a new game, giving it a name and both teams names. Needs a group?
+	Requires editorid.
+*/
+type CreateGameRequest struct {
+	GroupId     int    `json:"groupid,omitempty"`
+	EditorId    int    `json:"editorid"`
+	Name        string `json:"name"`
+	TeamOneName string `json:"teamonename"`
+	TeamTwoName string `json:"teamtwoname"`
+}
+
+/*
+	/game/changename/
+	Changes the name of a game. Requires editorId.
+*/
+type GameChangeNameRequest struct {
+	GameId   int    `json:"gameid"`
+	NewName  string `json:"newname"`
+	EditorId int    `json:"editorid"`
+}
+
+/*
+	/game/teamonescore/
+	Increments team one's score. Requires editorId.
+*/
+type TeamOneScoreRequest struct {
+	GameId   int `json:"gameid"`
+	EditorId int `json:"editorid"`
+}
+
+/*
+	/game/teamtwoscore/
+	Increments team two's score. Requires editorId.
+*/
+type TeamTwoScoreRequest struct {
+	GameId   int `json:"gameid"`
+	EditorId int `json:"editorid"`
+}
+
+/*
+	/game/completegame/
+	Marks game as complete. Requires editorId.
+*/
+type GameCompletedRequest struct {
+	GameId   int `json:"gameid"`
+	EditorId int `json:"editorid"`
+}
+
+/*
+	/game/switchserver/
+	Chooses serving team. Requires editorId.
+*/
+type GameSwitchServer struct {
+	GameId         int  `json:"gameid"`
+	EditorId       int  `json:"editorid"`
+	TeamOneServing bool `json:"teamoneserving"`
+}
+
+/*
+	/game/update/
+	Updates the entire Game row. Requires editor user.
+*/
+type UpdateGameRequest struct {
+	GameId         int       `json:"gameid"`
+	EditorId       int       `json:"editorid"`
+	Name           string    `json:"name"`
+	Date           time.Time `json:"date"`
+	TeamOneName    string    `json:"teamonename"`
+	TeamTwoName    string    `json:"teamtwoname"`
+	TeamOneScore   int       `json:"teamonescore"`
+	TeamTwoScore   int       `json:"teamtwoscore"`
+	TeamOneServing bool      `json:"teamoneserving"`
+}
